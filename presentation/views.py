bc858a4667b31b4651ec4dac4b767484326fab55
lacina <lacina4698@gmail.com>
2025-07-20 12:22:48 +0000

    Mise à jour pages accueil, contact, about, views et settings

--- presentation/views.py
+++ presentation/views.py
@@ -1,3 +1,4 @@
+from django.http import BadHeaderError
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.conf import settings
@@ -19,17 +20,21 @@ def projects(request):
     return render(request, 'presentation/projects.html')
 
 def contact(request):
-    message_sent = False
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        message = request.POST.get('message')
+    context = {}
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
 
         subject = f"Nouveau message de {name}"
         full_message = f"Nom: {name}\nEmail: {email}\n\nMessage:\n{message}"
-        
-        ssl._create_default_https_context = lambda: ssl.create_default_context(cafile=certifi.where())
-        send_mail(subject, full_message, settings.EMAIL_HOST_USER, ['lacina4698@gmail.com'])
-        message_sent = True
 
-    return render(request, 'presentation/contact.html', {'message_sent': message_sent})
+        try:
+            send_mail(subject, full_message, settings.DEFAULT_FROM_EMAIL, ['lacina4698@gmail.com'])
+            context["message_sent"] = True
+        except BadHeaderError:
+            context["error"] = True
+        except Exception:
+            context["error"] = True
+
+    return render(request, 'presentation/contact.html', context)
